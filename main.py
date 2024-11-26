92706ab8cd4402eef8a72e32f28b4f6cc0f24f56
PacurariuIM <jonny_boy90609@yahoo.com>
2024-11-26 16:24:21 +0100

    fix scaling for small screens

--- main.py
+++ main.py
@@ -149,8 +149,8 @@ def recipe():
     return redirect(url_for('get_recipes'))
 
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
-
 # if __name__ == '__main__':
-#     app.run(debug=True)
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+if __name__ == '__main__':
+    app.run(debug=True)

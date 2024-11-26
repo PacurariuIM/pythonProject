130b21778a929a7d2ae380055f0bbcd1709faf1c
PacurariuIM <jonny_boy90609@yahoo.com>
2024-11-26 18:32:06 +0100

    frontend more tuning

--- main.py
+++ main.py
@@ -16,21 +16,6 @@ db = SQLAlchemy(app)
 # Load environment variables from .env file
 load_dotenv()
 
-# Recipe model
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    name = db.Column(db.String(200), nullable=False)
-    ingredients = db.Column(db.Text, nullable=True)      # Text to store a list of ingredients
-    instructions = db.Column(db.Text, nullable=True)      # Text to store instructions
-    calories = db.Column(db.Integer, nullable=True)       # Calories information
-    image_url = db.Column(db.String(300), nullable=True)  # Store image URL
-    recipe_url = db.Column(db.String(300), nullable=True) # Original recipe URL
-
-    def __repr__(self):
-        return f"Recipe('{self.name}', '{self.user_id}')"
-
-
 # Debugging table
 @app.route('/db_check')
 def db_check():
